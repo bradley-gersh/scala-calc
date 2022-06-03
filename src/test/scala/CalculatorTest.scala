1696a6694812e3a0f7c0bc054ffb0de7c9457056
Bradley Gersh <bradley.gersh@bitrock.it>
2022-06-03 09:46:23 +0200

    test: first test runs with correct src directory

--- src/test/scala/CalculatorTest.scala
+++ src/test/scala/CalculatorTest.scala
@@ -1,5 +1,7 @@
 import org.scalatest.funsuite.AnyFunSuite
 
+import it.scalalearn.calculator.Calculator
+
 class CalculatorTest extends AnyFunSuite {
   test("Calculator.expr") {
     assert(Calculator.expr(3) === 27.0)
