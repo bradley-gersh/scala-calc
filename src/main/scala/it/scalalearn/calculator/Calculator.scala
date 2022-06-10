1de3d1b249cc58f662ff4a0d24dfdfd6520cdcda
Bradley Gersh <bradley.gersh@bitrock.it>
2022-06-10 13:01:32 +0200

    refactor: separated custom exceptions to own file

--- src/main/scala/it/scalalearn/calculator/Calculator.scala
+++ src/main/scala/it/scalalearn/calculator/Calculator.scala
@@ -62,15 +62,3 @@ object Calculator {
     } yield (value, tree)
   }
 }
-
-class CalculatorException(private val message: String) extends RuntimeException(message)
-
-class EvaluatorException(private val message: String) extends CalculatorException(message)
-
-class ParserException(private val message: String) extends CalculatorException(message)
-
-class LexerException(private val message: String) extends CalculatorException(message)
-
-class UnknownTokenException(private val message: String) extends LexerException(message) {
-  override def getMessage: String = s"unrecognized character: $message"
-}
