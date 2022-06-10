8a7811ed3994ec480c8b6354a6c993018f5baa0f
Bradley Gersh <bradley.gersh@bitrock.it>
2022-06-10 12:59:29 +0200

    refactor: amended parser tests to be independent of printer

--- src/test/scala/it/scalalearn/calculator/ParserTest.scala
+++ src/test/scala/it/scalalearn/calculator/ParserTest.scala
@@ -60,7 +60,7 @@ class ParserTest extends AnyFunSuite {
         Token(TokenType.NUMBER, "4"),
         Token(TokenType.STAR, "*"),
         Token(TokenType.NUMBER, "3"))) ===
-      Success(TermNode(
+      Success(FactorNode(
         Token(TokenType.STAR, "*"),
         NumberNode(4),
         NumberNode(3))))
@@ -70,7 +70,7 @@ class ParserTest extends AnyFunSuite {
         Token(TokenType.NUMBER, "4"),
         Token(TokenType.SLASH, "/"),
         Token(TokenType.NUMBER, "3"))) ===
-      Success(TermNode(
+      Success(FactorNode(
         Token(TokenType.SLASH, "/"),
         NumberNode(4),
         NumberNode(3))))
@@ -84,121 +84,275 @@ class ParserTest extends AnyFunSuite {
         Token(TokenType.NUMBER, "4"),
         Token(TokenType.PLUS, "+"),
         Token(TokenType.NUMBER, "0.5"),
-        Token(TokenType.PLUS, "-"),
+        Token(TokenType.DASH, "-"),
         Token(TokenType.NUMBER, "6"),
-        Token(TokenType.PLUS, "-"),
+        Token(TokenType.DASH, "-"),
         Token(TokenType.NUMBER, "2"),
         Token(TokenType.PLUS, "+"),
         Token(TokenType.NUMBER, "4"))) ===
       Success(
-        "(+ (- (- (+ 4.0 0.5) 6.0) 2.0) 4.0)"))
+        TermNode(
+          Token(TokenType.PLUS, "+"),
+          TermNode(
+            Token(TokenType.DASH, "-"),
+            TermNode(
+              Token(TokenType.DASH, "-"),
+              TermNode(
+                Token(TokenType.PLUS, "+"),
+                NumberNode(4),
+                NumberNode(0.5)),
+              NumberNode(6)),
+            NumberNode(2)),
+          NumberNode(4))))
   }
 
   test("Parser should parse multi-factor multiplication and division") {
 
     // 4 * 0.5 / 6 * 2 / 4
-    assert(Parser(List(
-      Token(TokenType.NUMBER, "4"),
-      Token(TokenType.STAR, "*"),
-      Token(TokenType.NUMBER, "0.5"),
-      Token(TokenType.SLASH, "/"),
-      Token(TokenType.NUMBER, "6"),
-      Token(TokenType.STAR, "*"),
-      Token(TokenType.NUMBER, "2"),
-      Token(TokenType.SLASH, "/"),
-      Token(TokenType.NUMBER, "4"))).map(_.toString) === Success("(/ (* (/ (* 4.0 0.5) 6.0) 2.0) 4.0)"))
+    assert(
+      Parser(List(
+        Token(TokenType.NUMBER, "4"),
+        Token(TokenType.STAR, "*"),
+        Token(TokenType.NUMBER, "0.5"),
+        Token(TokenType.SLASH, "/"),
+        Token(TokenType.NUMBER, "6"),
+        Token(TokenType.STAR, "*"),
+        Token(TokenType.NUMBER, "2"),
+        Token(TokenType.SLASH, "/"),
+        Token(TokenType.NUMBER, "4"))) ===
+      Success(
+        FactorNode(
+          Token(TokenType.SLASH, "/"),
+          FactorNode(
+            Token(TokenType.STAR, "*"),
+            FactorNode(
+              Token(TokenType.SLASH, "/"),
+              FactorNode(
+                Token(TokenType.STAR, "*"),
+                NumberNode(4),
+                NumberNode(0.5)),
+              NumberNode(6)),
+            NumberNode(2)),
+          NumberNode(4))))
   }
 
   test("Parser should handle order of operations in expressions without parentheses") {
 
     // 4 * 0.5 + 6
-    assert(Parser(List(
-      Token(TokenType.NUMBER, "4"),
-      Token(TokenType.STAR, "*"),
-      Token(TokenType.NUMBER, "0.5"),
-      Token(TokenType.PLUS, "+"),
-      Token(TokenType.NUMBER, "6"))).map(_.toString) === Success("(+ (* 4.0 0.5) 6.0)"))
+    assert(
+      Parser(List(
+        Token(TokenType.NUMBER, "4"),
+        Token(TokenType.STAR, "*"),
+        Token(TokenType.NUMBER, "0.5"),
+        Token(TokenType.PLUS, "+"),
+        Token(TokenType.NUMBER, "6"))) ===
+      Success(TermNode(
+        Token(TokenType.PLUS, "+"),
+        FactorNode(
+          Token(TokenType.STAR, "*"),
+          NumberNode(4),
+          NumberNode(0.5)),
+        NumberNode(6))))
 
     // 4 + 0.5 * 6
-    assert(Parser(List(
-      Token(TokenType.NUMBER, "4"),
-      Token(TokenType.PLUS, "+"),
-      Token(TokenType.NUMBER, "0.5"),
-      Token(TokenType.STAR, "*"),
-      Token(TokenType.NUMBER, "6"))).map(_.toString) === Success("(+ 4.0 (* 0.5 6.0))"))
+    assert(
+      Parser(List(
+        Token(TokenType.NUMBER, "4"),
+        Token(TokenType.PLUS, "+"),
+        Token(TokenType.NUMBER, "0.5"),
+        Token(TokenType.STAR, "*"),
+        Token(TokenType.NUMBER, "6"))) ===
+      Success(TermNode(
+        Token(TokenType.PLUS, "+"),
+        NumberNode(4),
+        FactorNode(
+          Token(TokenType.STAR, "*"),
+          NumberNode(0.5),
+          NumberNode(6)))))
 
     // 4 + 0.5 * 6 - 2 / 4 * 9
-    assert(Parser(List(
-      Token(TokenType.NUMBER, "4"),
-      Token(TokenType.PLUS, "+"),
-      Token(TokenType.NUMBER, "0.5"),
-      Token(TokenType.STAR, "*"),
-      Token(TokenType.NUMBER, "6"),
-      Token(TokenType.DASH, "-"),
-      Token(TokenType.NUMBER, "2"),
-      Token(TokenType.SLASH, "/"),
-      Token(TokenType.NUMBER, "4"),
-      Token(TokenType.STAR, "*"),
-      Token(TokenType.NUMBER, "9"),
-    )).map(_.toString) === Success("(- (+ 4.0 (* 0.5 6.0)) (* (/ 2.0 4.0) 9.0))"))
+    assert(
+      Parser(List(
+        Token(TokenType.NUMBER, "4"),
+        Token(TokenType.PLUS, "+"),
+        Token(TokenType.NUMBER, "0.5"),
+        Token(TokenType.STAR, "*"),
+        Token(TokenType.NUMBER, "6"),
+        Token(TokenType.DASH, "-"),
+        Token(TokenType.NUMBER, "2"),
+        Token(TokenType.SLASH, "/"),
+        Token(TokenType.NUMBER, "4"),
+        Token(TokenType.STAR, "*"),
+        Token(TokenType.NUMBER, "9"))) ===
+      Success(TermNode(
+        Token(TokenType.DASH, "-"),
+        TermNode(
+          Token(TokenType.PLUS, "+"),
+          NumberNode(4.0),
+          FactorNode(
+            Token(TokenType.STAR, "*"),
+            NumberNode(0.5),
+            NumberNode(6))),
+        FactorNode(
+          Token(TokenType.STAR, "*"),
+          FactorNode(
+            Token(TokenType.SLASH, "/"),
+            NumberNode(2.0),
+            NumberNode(4.0)),
+          NumberNode(9)))))
   }
 
   test("Parser should prioritize parentheses over other operators") {
 
     // 4 * (0.5 + 6)
-    assert(Parser(List(
-      Token(TokenType.NUMBER, "4"),
-      Token(TokenType.STAR, "*"),
-      Token(TokenType.LPAREN, "("),
-      Token(TokenType.NUMBER, "0.5"),
-      Token(TokenType.PLUS, "+"),
-      Token(TokenType.NUMBER, "6"),
-      Token(TokenType.RPAREN, ")"))).map(_.toString) === Success("(* 4.0 (+ 0.5 6.0))"))
+    assert(
+      Parser(List(
+        Token(TokenType.NUMBER, "4"),
+        Token(TokenType.STAR, "*"),
+        Token(TokenType.LPAREN, "("),
+        Token(TokenType.NUMBER, "0.5"),
+        Token(TokenType.PLUS, "+"),
+        Token(TokenType.NUMBER, "6"),
+        Token(TokenType.RPAREN, ")"))) ===
+      Success(FactorNode(
+        Token(TokenType.STAR, "*"),
+        NumberNode(4),
+        TermNode(
+          Token(TokenType.PLUS, "+"),
+          NumberNode(0.5),
+          NumberNode(6)))))
 
     // (4 + 0.5) * 6
-    assert(Parser(List(
-      Token(TokenType.LPAREN, "("),
-      Token(TokenType.NUMBER, "4"),
-      Token(TokenType.PLUS, "+"),
-      Token(TokenType.NUMBER, "0.5"),
-      Token(TokenType.RPAREN, ")"),
-      Token(TokenType.STAR, "*"),
-      Token(TokenType.NUMBER, "6"))).map(_.toString) === Success("(* (+ 4.0 0.5) 6.0)"))
+    assert(
+      Parser(List(
+        Token(TokenType.LPAREN, "("),
+        Token(TokenType.NUMBER, "4"),
+        Token(TokenType.PLUS, "+"),
+        Token(TokenType.NUMBER, "0.5"),
+        Token(TokenType.RPAREN, ")"),
+        Token(TokenType.STAR, "*"),
+        Token(TokenType.NUMBER, "6"))) ===
+      Success(FactorNode(
+        Token(TokenType.STAR, "*"),
+        TermNode(
+          Token(TokenType.PLUS, "+"),
+          NumberNode(4),
+          NumberNode(0.5)),
+        NumberNode(6))))
 
   }
 
   test("Parser should correctly handle nested parentheses") {
 
+    // (2 + (3 * (2 - 5) + (2)) / -7) + (5 - 3)
+    assert(
+      Parser(List(
+        Token(TokenType.LPAREN, "("),
+        Token(TokenType.NUMBER, "2"),
+        Token(TokenType.PLUS, "+"),
+        Token(TokenType.LPAREN, "("),
+        Token(TokenType.NUMBER, "3"),
+        Token(TokenType.STAR, "*"),
+        Token(TokenType.LPAREN, "("),
+        Token(TokenType.NUMBER, "2"),
+        Token(TokenType.DASH, "-"),
+        Token(TokenType.NUMBER, "5"),
+        Token(TokenType.RPAREN, ")"),
+        Token(TokenType.PLUS, "+"),
+        Token(TokenType.LPAREN, "("),
+        Token(TokenType.NUMBER, "2"),
+        Token(TokenType.RPAREN, ")"),
+        Token(TokenType.RPAREN, ")"),
+        Token(TokenType.SLASH, "/"),
+        Token(TokenType.DASH, "-"),
+        Token(TokenType.NUMBER, "7"),
+        Token(TokenType.RPAREN, ")"),
+        Token(TokenType.PLUS, "+"),
+        Token(TokenType.LPAREN, "("),
+        Token(TokenType.NUMBER, "5"),
+        Token(TokenType.DASH, "-"),
+        Token(TokenType.NUMBER, "3"),
+        Token(TokenType.RPAREN, ")"))) ===
+      Success(TermNode(
+        Token(TokenType.PLUS, "+"),
+        TermNode(
+          Token(TokenType.PLUS, "+"),
+          NumberNode(2),
+          FactorNode(
+            Token(TokenType.SLASH, "/"),
+            TermNode(
+              Token(TokenType.PLUS, "+"),
+              FactorNode(
+                Token(TokenType.STAR, "*"),
+                NumberNode(3),
+                TermNode(
+                  Token(TokenType.DASH, "-"),
+                  NumberNode(2),
+                  NumberNode(5))),
+              NumberNode(2)),
+            SignNode(
+              Token(TokenType.DASH, "-"),
+              NumberNode(7)))),
+        TermNode(
+          Token(TokenType.DASH, "-"),
+          NumberNode(5),
+          NumberNode(3)))))
+
     // 4 * (0.5 + ((6 - 2 * 2) / ((4 * 4) - 0.5) + 9))
-    assert(Parser(List(
-      Token(TokenType.NUMBER, "4"),
-      Token(TokenType.STAR, "*"),
-      Token(TokenType.LPAREN, "("),
-      Token(TokenType.NUMBER, "0.5"),
-      Token(TokenType.PLUS, "+"),
-      Token(TokenType.LPAREN, "("),
-      Token(TokenType.LPAREN, "("),
-      Token(TokenType.NUMBER, "6"),
-      Token(TokenType.DASH, "-"),
-      Token(TokenType.NUMBER, "2"),
-      Token(TokenType.STAR, "*"),
-      Token(TokenType.NUMBER, "2"),
-      Token(TokenType.RPAREN, ")"),
-      Token(TokenType.SLASH, "/"),
-      Token(TokenType.LPAREN, "("),
-      Token(TokenType.LPAREN, "("),
-      Token(TokenType.NUMBER, "4"),
-      Token(TokenType.STAR, "*"),
-      Token(TokenType.NUMBER, "4"),
-      Token(TokenType.RPAREN, ")"),
-      Token(TokenType.DASH, "-"),
-      Token(TokenType.NUMBER, "0.5"),
-      Token(TokenType.RPAREN, ")"),
-      Token(TokenType.PLUS, "+"),
-      Token(TokenType.NUMBER, "9"),
-      Token(TokenType.RPAREN, ")"),
-      Token(TokenType.RPAREN, ")"),
-    )).map(_.toString) === Success("(* 4.0 (+ 0.5 (+ (/ (- 6.0 (* 2.0 2.0)) (- (* 4.0 4.0) 0.5)) 9.0)))"))
+    assert(
+      Parser(List(
+        Token(TokenType.NUMBER, "4"),
+        Token(TokenType.STAR, "*"),
+        Token(TokenType.LPAREN, "("),
+        Token(TokenType.NUMBER, "0.5"),
+        Token(TokenType.PLUS, "+"),
+        Token(TokenType.LPAREN, "("),
+        Token(TokenType.LPAREN, "("),
+        Token(TokenType.NUMBER, "6"),
+        Token(TokenType.DASH, "-"),
+        Token(TokenType.NUMBER, "2"),
+        Token(TokenType.STAR, "*"),
+        Token(TokenType.NUMBER, "2"),
+        Token(TokenType.RPAREN, ")"),
+        Token(TokenType.SLASH, "/"),
+        Token(TokenType.LPAREN, "("),
+        Token(TokenType.LPAREN, "("),
+        Token(TokenType.NUMBER, "4"),
+        Token(TokenType.STAR, "*"),
+        Token(TokenType.NUMBER, "4"),
+        Token(TokenType.RPAREN, ")"),
+        Token(TokenType.DASH, "-"),
+        Token(TokenType.NUMBER, "0.5"),
+        Token(TokenType.RPAREN, ")"),
+        Token(TokenType.PLUS, "+"),
+        Token(TokenType.NUMBER, "9"),
+        Token(TokenType.RPAREN, ")"),
+        Token(TokenType.RPAREN, ")"))) ===
+      Success(FactorNode(
+        Token(TokenType.STAR, "*"),
+        NumberNode(4),
+        TermNode(
+          Token(TokenType.PLUS, "+"),
+          NumberNode(0.5),
+          TermNode(
+            Token(TokenType.PLUS, "+"),
+            FactorNode(
+              Token(TokenType.SLASH, "/"),
+              TermNode(
+                Token(TokenType.DASH, "-"),
+                NumberNode(6),
+                FactorNode(
+                  Token(TokenType.STAR, "*"),
+                  NumberNode(2),
+                  NumberNode(2))),
+              TermNode(
+                Token(TokenType.DASH, "-"),
+                FactorNode(
+                  Token(TokenType.STAR, "*"),
+                  NumberNode(4),
+                  NumberNode(4)),
+                NumberNode(0.5))),
+            NumberNode(9))))))
   }
 
   test("Parser should fail if it receives adjacent numbers without an operator or parentheses") {
