980a7968994196257ff6af27939df54c49ae035d
Bradley Gersh <bradley.gersh@bitrock.it>
2022-06-13 11:03:17 +0200

    refactor: lexer and parser now use Either

--- src/main/scala/it/scalalearn/calculator/Lexer.scala
+++ src/main/scala/it/scalalearn/calculator/Lexer.scala
@@ -2,7 +2,6 @@ package it.scalalearn.calculator
 
 import scala.annotation.tailrec
 import scala.collection.mutable.ArrayBuffer
-import scala.util.Try
 
 /**
  * Lexer singleton to scan calculator input into tokens.
@@ -13,10 +12,10 @@ object Lexer {
    * Public access to the Lexer object
    *
    * @param  input  string to be lexed
-   * @return        Try object wrapping the list of tokens found in the input
+   * @return        Either an error message or the list of tokens found in the input
    */
-  def apply(input: String): Try[List[Token]] = {
-    Try(read(input.toList, List[Token]()))
+  def apply(input: String): Either[String, List[Token]] = {
+    read(input.toList, List[Token]())
   }
 
   /**
@@ -31,34 +30,34 @@ object Lexer {
    *
    * @param  input  list of the characters yet to be processed
    * @param  tokens list of the tokens processed so far
-   * @return        a list of processed tokens
+   * @return        Either an error message or a list of processed tokens
    */
   @tailrec
-  private def read(input: List[Char], tokens: List[Token]): List[Token] = {
+  private def read(input: List[Char], tokens: List[Token]): Either[String, List[Token]] = {
     input match {
       // Finished processing
-      case Nil => tokens.reverse
+      case Nil => Right(tokens.reverse)
 
       // Ignore whitespace
       case WS() :: rest => read(rest, tokens)
 
       // Number literals
       case (first @ (DIGITS() | SEPARATOR)) +: rest =>
-        val (newRest, numberToken) = readNumberToken(rest, List(first), first == SEPARATOR)
-        read(newRest, numberToken +: tokens)
+        readNumberToken(rest, List(first), first == SEPARATOR) match {
+          case Left(error) => Left(error)
+          case Right(newRest, numberToken) => read(newRest, numberToken +: tokens)
+        }
 
       // One-character patterns
-      case first +: rest =>
-        val newToken = first match {
-          case '(' => LPAREN
-          case ')' => RPAREN
-          case '+' => PLUS
-          case '-' => DASH
-          case '*' => STAR
-          case '/' => SLASH
-          case _ => throw new UnknownTokenException(first.toString)
-        }
-        read(rest, newToken +: tokens)
+      case first +: rest => first match {
+        case '(' => read(rest, LPAREN +: tokens)
+        case ')' => read(rest, RPAREN +: tokens)
+        case '+' => read(rest, PLUS +: tokens)
+        case '-' => read(rest, DASH +: tokens)
+        case '*' => read(rest, STAR +: tokens)
+        case '/' => read(rest, SLASH +: tokens)
+        case unknown => Left(s"unrecognized character: $unknown")
+      }
     }
   }
 
@@ -68,22 +67,22 @@ object Lexer {
    * @param  input          list of the characters yet to be processed
    * @param  currToken      list of the characters to be wrapped in the current token
    * @param  fractionalPart currently lexing the fractional part of a decimal number
-   * @return                a 2-tuple containing both the remaining characters to be processed and the
-   *                        newly generated Number token.
+   * @return                Either an error message or a 2-tuple containing both the remaining
+   *                        characters to be processed and the newly generated Number token.
    */
   @tailrec
-  private def readNumberToken(input: List[Char], currToken: List[Char], fractionalPart: Boolean): (List[Char], Token) = {
+  private def readNumberToken(input: List[Char], currToken: List[Char], fractionalPart: Boolean): Either[String, (List[Char], Token)] = {
     def flushNumber() = {
       val numberString = currToken.reverse.mkString
-      if (numberString == SEPARATOR.toString) throw new LexerException("isolated . not permitted")
-      else (input, NUMBER(numberString))
+      if (numberString == SEPARATOR.toString) Left("isolated . not permitted")
+      else Right(input, NUMBER(numberString))
     }
 
     input match {
       case (first @ DIGITS()) :: rest => readNumberToken(rest, first :: currToken, fractionalPart)
       case (first @ SEPARATOR) +: rest =>
         if (!fractionalPart) readNumberToken(rest, first +: currToken, true)
-        else throw new LexerException(s"only one '$SEPARATOR' character permitted per number")
+        else Left(s"only one '$SEPARATOR' character permitted per number")
       case _ => flushNumber()
     }
   }
