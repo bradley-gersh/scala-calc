64929a1584f7717312202404e982313fbd023a1e
Bradley Gersh <bradley.gersh@bitrock.it>
2022-06-13 16:07:19 +0200

    feat: expanded exceptions hierarchy

--- src/main/scala/it/scalalearn/calculator/exceptions.scala
+++ src/main/scala/it/scalalearn/calculator/exceptions.scala
@@ -6,6 +6,18 @@ class EvaluatorException(private val message: String) extends CalculatorExceptio
 
 class ParserException(private val message: String) extends CalculatorException(message)
 
+class InfiniteValueException(private val message: String) extends ParserException(message)
+
+class EmptyNodeException(private val message: String) extends ParserException(message)
+
+class DivisionByZeroException(private val message: String) extends ParserException(message)
+
+class InvalidOperatorException(private val message: String) extends ParserException(message)
+
+class UnparsedTokensException(private val message: String) extends ParserException(message)
+
+class UnmatchedParenthesesException(private val message: String) extends ParserException(message)
+
 class LexerException(private val message: String) extends CalculatorException(message)
 
 class UnknownTokenException(private val message: String) extends LexerException(message) {
