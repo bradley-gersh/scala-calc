6daf4f8bb922cfe25f55ffcbbd9e6ca4d8e4048b
Bradley Gersh <bradley.gersh@bitrock.it>
2022-06-13 12:51:34 +0200

    refactor: calculator and evaluator now use Either instead of Try

--- src/main/scala/it/scalalearn/calculator/Evaluator.scala
+++ src/main/scala/it/scalalearn/calculator/Evaluator.scala
@@ -1,67 +1,55 @@
 package it.scalalearn.calculator
 
-import scala.util.Try
-
 /**
  * Evaluates a given parse tree for the return value
  */
-object Evaluator extends Function[ParseNode, Try[Double]] {
+object Evaluator extends Function[ParseNode, Either[String, Double]] {
 
   /**
    * Public access to the Evaluator function
    *
    * @param  tree  root node of the parse tree
-   * @return       Try of the numerical value associated with evaluating the parse tree
+   * @return       Either an error message or the numerical value of the parse tree
    */
-  def apply(tree: ParseNode): Try[Double] = {
-    val value = Try(evaluate(tree))
-
-    if (value.isSuccess) {
-      if (value.get.isInfinite) Try(throw new EvaluatorException("infinite value obtained"))
-      else if (value.get == -0.0) Try(0.0) // the JVM allows -0.0, but we will not
-      else value
-    } else value
+  def apply(tree: ParseNode): Either[String, Double] = {
+    evaluate(tree) match {
+      case Left(error) => Left(error)
+      case Right(value) if value.isInfinite => Left("infinite value obtained")
+      case Right(value) if value == -0.0 => Right(0.0)
+      case Right(value) => Right(value)
+    }
   }
 
   /**
    * Recursively evaluates the numerical value of a parse tree
    *
-   * @return                           computed value of the parse tree
-   * @throws  IllegalArgumentException if a NaN value is obtained
-   * @throws  ParserException          if an infinite value is obtained
-   * @throws  ParserException          if the parse tree has an empty node
-   * @throws  ParserException          in case of division by 0
-   * @throws  ParserException          if an operator node contains an invalid operator
-   *
+   * @return  Either an error message or the computed value of the parse tree (possibly infinite)
    */
-  def evaluate: ParseNode => Double = {
-    case EmptyNode => throw new ParserException("incomplete input; missing a sub-expression")
+  def evaluate: ParseNode => Either[String, Double] = {
+    case EmptyNode => Left("incomplete input; missing a sub-expression")
 
     case NumberNode(value) =>
       // a NaN value should not be found if the lexer works, so this will not be handled as a routine ParserException
-      if (value.isNaN) throw new IllegalArgumentException("NaN value evaluated")
-      else if (value.isInfinite) throw new ParserException("infinite value evaluated")
-      else value
+      if (value.isNaN) Left("NaN value evaluated")
+      else if (value.isInfinite) Left("infinite value evaluated")
+      else Right(value)
 
     case SignNode(PLUS, expr) => evaluate(expr)
-    case SignNode(DASH, expr) => -evaluate(expr)
-    case SignNode(op, _) => throw new ParserException(s"invalid unary operator `${op.string}`")
+    case SignNode(DASH, expr) => evaluate(expr).map(-_)
+    case SignNode(op, _) => Left(s"invalid unary operator `${op.string}`")
 
-    case FactorNode(STAR, expr1, expr2) => evaluate(expr1) * evaluate(expr2)
+    case FactorNode(STAR, expr1, expr2) => evaluate(expr1).flatMap(val1 => evaluate(expr2).map(val2 => val1 * val2))
     case FactorNode(SLASH, expr1, expr2) =>
-      val numerator = evaluate(expr1)
-      val denominator = evaluate(expr2)
+      evaluate(expr1).flatMap(numerator => evaluate(expr2).map(denominator => (numerator, denominator) match {
+//        case (0.0, 0.0) => Left("indeterminate form 0/0 obtained")
+//        case (_, 0.0) => Left("division by 0")
+        case _ => numerator / denominator
+      }))
 
-      if (denominator == 0.0) {
-        if (numerator == 0.0) throw new ParserException("indeterminate form 0/0 obtained")
-        else throw new ParserException("division by zero")
-      } else numerator / denominator
-    case FactorNode(op, _, _) =>
-      throw new ParserException(s"improper operation ${op.string} where multiplication or division was expected")
+    case FactorNode(op, _, _) => Left(s"improper operation ${op.string} where multiplication or division was expected")
 
-    case TermNode(PLUS, expr1, expr2) => evaluate(expr1) + evaluate(expr2)
-    case TermNode(DASH, expr1, expr2) => evaluate(expr1) - evaluate(expr2)
-    case TermNode(op, _, _) =>
-      throw new ParserException(s"improper operation ${op.string} where addition or subtraction was expected")
+    case TermNode(PLUS, expr1, expr2) => evaluate(expr1).flatMap(val1 => evaluate(expr2).map(val2 => val1 + val2))
+    case TermNode(DASH, expr1, expr2) => evaluate(expr1).flatMap(val1 => evaluate(expr2).map(val2 => val1 - val2))
+    case TermNode(op, _, _) => Left(s"improper operation ${op.string} where addition or subtraction was expected")
   }
 }
